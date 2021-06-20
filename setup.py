6ffe93b05cc6fb4ff63a21a080c9402a1b76988c
trickstertwo <trickstertwo@gmail.com>
2021-06-20 21:10:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,7 @@
 
 from setuptools import setup, find_packages
 
-try:
-    from pip._internal.download import PipSession
-    pip_session = PipSession()
-except ImportError:
-    from pip._internal.network.session import PipSession
-    pip_session = PipSession()
-        
+from pip._internal.network.session import PipSession
 from pip._internal.req import parse_requirements
 
 
